cbe4fcc25b7d1173ada46b43c3ffb3dfc14b7457
tbeaumont79 <tbeaumont79@icloud.com>
2025-08-24 09:14:34 +0200

    feat: add a reactive form for login

--- kata-secu-tenant-front/src/app/pages/login/login.ts
+++ kata-secu-tenant-front/src/app/pages/login/login.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
 })
 export class Login {
+  profileForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
 
+  onSubmit() {
+    console.log(this.profileForm.value);
+  }
 }
